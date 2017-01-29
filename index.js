c86edf3d9ca6ea629fe4143f59282947eaafdb2f
Rotem Yaari <vmalloc@gmail.com>
2017-01-29 21:00:52 +0200

    Initial version

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 /* jshint node: true */
 'use strict';
 
+var syncExec = require('sync-exec');
+
 module.exports = {
-  name: 'ember-cli-hook-scripts'
+    name: 'ember-cli-hook-scripts',
+
+    preBuild() {
+        this._exec('preBuild');
+    },
+
+    postBuild() {
+        this._exec('postBuild');
+    },
+
+    buildError() {
+        this._exec('buildError');
+    },
+
+    postBuild() {
+        this._exec('postBuild');
+    },
+
+
+    _exec(hookName) {
+        var full_path = '~/.config/ember/hooks/' + hookName;
+
+        syncExec(full_path);
+    },
+
 };
